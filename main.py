5bd1bf9ffbd1cb9f47ee36f26cd34cdd07ffb5ae
casalecchi <felipevilela191@gmail.com>
2024-01-28 15:36:15 -0300

    data to csv done

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ json_url = f"https://api.sofascore.com/api/v1/event/{match_id}/lineups"
 response = requests.get(json_url, headers=HEADERS)
 data = response.json()
 
+home_df = pd.DataFrame(columns=('name', 'position', 'aerial_won', 'ground_won', 'clearences', 'interceptions'))
 home_team = data["home"]
 home_players = home_team["players"]
-for player in home_players:
+for index, player in enumerate(home_players):
     name = player["player"]["name"]
     position = player["position"]
-    statistics = player["statistics"]
-    aerial_won = statistics["aerialWon"]
-    ground_won = statistics["duelWon"] - aerial_won
-    total_clearence = statistics["totalClearance"]
-    interceptions = statistics["interceptionWon"]
-    break
+    statistics = player.get("statistics", {})
+    aerial_won = statistics.get("aerialWon", 0)
+    ground_won = statistics.get("duelWon", 0) - aerial_won
+    total_clearence = statistics.get("totalClearance", 0)
+    interceptions = statistics.get("interceptionWon", 0)
+    home_df.loc[index] = [name, position, aerial_won, ground_won, total_clearence, interceptions]
+
+home_df.to_csv('teste.csv')
 
