31de6f21959e279b0f250e44b18fa1bfb096613d
casalecchi <felipevilela191@gmail.com>
2024-02-02 09:24:57 -0300

    generate ranking and data

--- generate_chart.py
+++ generate_chart.py
@@ -2,4 +2,7 @@ from utils.charts import *
 
 path = os.getcwd() + '/data/data.csv'
 data = pd.read_csv(path)
-generate_bar_from_data(data, "accuratePass", "Ranking de passes certos na rodada 4 - Carioca")
+generate_bar_from_data(data, "totalPass", "Ranking de passes certos na rodada 4 - Carioca")
+generate_bar_from_data(data, "duelWon", "Ranking de duelos ganhos na rodada 4 - Carioca")
+generate_bar_from_data(data, "interceptionWon", "Ranking de interceptações na rodada 4 - Carioca")
+
