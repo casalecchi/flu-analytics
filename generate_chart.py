3bddaed67234ab8c45b6b721d8c584ea64bf614c
casalecchi <felipevilela191@gmail.com>
2024-02-03 10:44:25 -0300

    analysis of round 5

--- generate_chart.py
+++ generate_chart.py
@@ -2,7 +2,6 @@ from utils.charts import *
 
 path = os.getcwd() + '/data/data.csv'
 data = pd.read_csv(path)
-generate_bar_from_data(data, "totalPass", "Ranking de passes certos na rodada 4 - Carioca")
-generate_bar_from_data(data, "duelWon", "Ranking de duelos ganhos na rodada 4 - Carioca")
-generate_bar_from_data(data, "interceptionWon", "Ranking de interceptações na rodada 4 - Carioca")
-
+generate_bar_from_data(data, "totalPass", "Ranking de passes certos na rodada 5 - Carioca")
+generate_bar_from_data(data, "duelWon", "Ranking de duelos ganhos na rodada 5 - Carioca")
+generate_bar_from_data(data, "interceptionWon", "Ranking de interceptações na rodada 5 - Carioca")
