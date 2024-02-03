806a8628acf0bc00e6335df89d326825128fc125
casalecchi <felipevilela191@gmail.com>
2024-02-03 16:52:06 -0300

    change header: working

--- utils/__init__.py
+++ utils/__init__.py
@@ -12,5 +12,5 @@ from utils.Statistics import *
 from utils.Team import *
 
 HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
 }
